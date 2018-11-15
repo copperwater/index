1be7f96415ebf1a49f22b2b6159931bdd812079d
chengj6 <chengj6@rpi.edu>
2018-11-15 11:37:16 -0500

    Add ServerObject super class

--- api/serverObjects.py
+++ api/serverObjects.py
@@ -1,30 +1,37 @@
+class serverObject:
 
-class text_Transformation:
+    def __init__(self, name):
+        self.name = name
+        
+    def toParam(self):
+        return [self]
 
-    def __init__(dict):
+class text_Transformation(serverObject):
+
+    def __init__(self, dict):
         self.meta_data = dict[Metadata]
         self.text = dict[text]
         self.grams = dict[grams]
 
-    def toParam():
+    def toParam(self):
         return [self.meta_data, self.text, self.grams]
 
-class link_Analysis():
+class link_Analysis(serverObject):
 
-    def __init__(dict):
+    def __init__(self, dict):
         self.url = dict[url]
         self.page_rank = dict[pagerank]
         self.inlinks = dict[inlinks]
 
-    def toParam():
+    def toParam(self):
         return [self.url, self.pagerank, self.inlinks]
 
-class crawling():
+class crawling(serverObject):
 
-    def __init___(dict):
+    def __init___(self, dict):
         self.url = dict[url]
         self.old_url = dict[old_url]
         self.operation = dict[operation]
 
-    def toParam():
+    def toParam(self):
         return [self.url, self.old_url, self.operation]
