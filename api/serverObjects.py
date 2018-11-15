09b089cd99649732afd67f5eefecd3bbcddf6590
chengj6 <chengj6@rpi.edu>
2018-11-15 00:32:17 -0500

    Files used for psycopg2 connect

---
+++ api/serverObjects.py
@@ -0,0 +1,21 @@
+
+class text_Transformation:
+
+    def __init__(dict):
+        self.meta_data = dict[Metadata]
+        self.text = dict[text]
+        self.grams = dict[grams]
+
+class link_Analysis():
+
+    def __init__(dict):
+        self.url = dict[url]
+        self.page_rank = dict[pagerank]
+        self.inlinks = dict[inlinks]
+
+class crawling():
+
+    def __init___(dict):
+        self.url = dict[url]
+        self.old_url = dict[old_url]
+        self.operation = dict[operation]
