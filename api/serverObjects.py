99d58fa0ce1bb263ce0161f646cbcd27e9b63177
chengj6 <chengj6@rpi.edu>
2018-11-19 10:35:05 -0500

    Someone Updated I need to sync so I'm sommiting

--- api/serverObjects.py
+++ api/serverObjects.py
@@ -9,13 +9,24 @@ class serverObject:
 class text_Transformation(serverObject):
 
     def __init__(self, dict):
+        self.dict = dict
         self.meta_data = dict[Metadata]
         self.text = dict[text]
         self.grams = dict[grams]
 
-    def query():
+    def query(self):
         # generic query structure for TextTransformation
-        return ""
+        parameter = (self.dict[Metadata][DOCID], self.dict[Metadata][url],
+          self.dict[Metadata][Title],
+          self.dict[Metadata][Descriptions],
+          self.dict[Text][headings],
+          self.dict[Text][body])
+        query = "insert into documents (id, url, title, description, sect_headings, paragraphs) values (%s, %s, %s,%s,%s,%s)"
+        return_dict = {query:parameter}
+        for ngram in self.dict[grams]
+            parameter =
+            query = "insert into index values(%s,%s,%s,%s,%s,%s)"
+        return
 
     def toParam(self):
         return [self.meta_data, self.text, self.grams]
