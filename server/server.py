6617eb96a09bd3a7f9819fbee01d371e2aec9ac0
J. Farnsworth <6502enthusiast@gmail.com>
2018-12-03 20:13:10 -0500

    Server now translates requests and invokes the API handlers

--- server/server.py
+++ server/server.py
@@ -1,10 +1,19 @@
+import sys
+sys.path.append("api")
+from server_Objects import text_Transformation, link_Analysis, crawling
+from connect import connect
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 
 PORT_NUMBER = 5350
 
-# Handle requests.
+sobjs = {
+    "/text_Transformation": text_Transformation,
+    "/link_Analysis": link_Analysis,
+    "/crawling": crawling
+}
 
+# Handle requests.
 
 class messageHandler(BaseHTTPRequestHandler):
 
@@ -12,18 +21,34 @@ class messageHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         contentLength = int(self.headers['Content-Length'])
         postData = self.rfile.read(contentLength)
+        
+        print("Request to %s" % self.path)
+        
+        apiHandler = sobjs.get(self.path)
+        
+        if apiHandler is None:
+            print('Warning: Unrecognized path %s' % self.path)
+        else:
+            message = json.loads(postData)
+            
+            print('On %s received %r' % (self.path, message))
+            
+            apiHandler.__init__(apiHandler, message)
+            
+            result = connect.insert(apiHandler)
+            
+            if result is "It Works":
+                self.send_response(200)
+                print("Handler returned without error. Responding with 200 OK")
+            else:
+                self.send_response(500)
+                print("Handler encountered an error. Responding with 500 Internal Server Error")
 
-        message = json.loads(postData)
+            message = {}
 
-        print('Received %r' % message)
-
-        self.send_response(200)
-
-        message = {'testkey': 'testresponse'}
-
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        self.wfile.write(json.dumps(message).encode('utf-8'))
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(json.dumps(message).encode('utf-8'))
         return
 
 
