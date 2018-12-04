0f0bf2f0fef7a0926010743062bc4c328e529805
J. Farnsworth <6502enthusiast@gmail.com>
2018-12-03 20:14:05 -0500

    Test client makes a basic request

--- testclient/client.py
+++ testclient/client.py
@@ -2,8 +2,8 @@ from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 import json
 
-url = 'http://localhost:5350/'
-data = {'testkey': 'testval'}
+url = 'http://localhost:5350/crawling'
+data = {'urls': 'http://google.com/'}
 
 request = Request(url)
 response = urlopen(request, json.dumps(data).encode('utf-8')).read()
